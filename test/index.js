77db6ca18079d32b24c9094106368c3130ae5657
RyotaKodaira <l98603345@gmail.com>
2018-02-19 18:39:03 +0900

    [Fix] transform testing Object.

--- test/index.js
+++ test/index.js
@@ -3,15 +3,8 @@ import { convertValidationError } from '../src/index'
 
 test('convert single depth errors', t => {
   const err = {
-    response: {
-      status: 422,
-      data: {
-        errors: {
-          name: ['The name field is required.'],
-          password: ['The password field is required.'],
-        },
-      },
-    },
+    name: ['The name field is required.'],
+    password: ['The password field is required.'],
   }
   t.deepEqual(convertValidationError(err), {
     name: ['The name field is required.'],
@@ -21,15 +14,8 @@ test('convert single depth errors', t => {
 
 test('convert nested errors', t => {
   const err = {
-    response: {
-      status: 422,
-      data: {
-        errors: {
-          'staff.id': ['The id field is required.'],
-          'staff.name': ['The name field is required.'],
-        },
-      },
-    },
+  'staff.id': ['The id field is required.'],
+  'staff.name': ['The name field is required.'],
   }
   t.deepEqual(convertValidationError(err), {
     staff: {
@@ -41,17 +27,10 @@ test('convert nested errors', t => {
 
 test('convert nested iterative errors', t => {
   const err = {
-    response: {
-      status: 422,
-      data: {
-        errors: {
-          'staff.0.id': ['The id field is required.'],
-          'staff.0.name': ['The name field is required.'],
-          'staff.1.id': ['The id field is required.'],
-          'staff.1.name': ['The name field is required.'],
-        },
-      },
-    },
+    'staff.0.id': ['The id field is required.'],
+    'staff.0.name': ['The name field is required.'],
+    'staff.1.id': ['The id field is required.'],
+    'staff.1.name': ['The name field is required.'],
   }
   t.deepEqual(convertValidationError(err), {
     staff: {
@@ -69,17 +48,10 @@ test('convert nested iterative errors', t => {
 
 test('convert nested iterative errors with default errMsg', t => {
   const err = {
-    response: {
-      status: 422,
-      data: {
-        errors: {
-          'staff.0.id': ['The id field is required.'],
-          'staff.0.name': ['The name field is required.'],
-          'staff.1.id': ['The id field is required.'],
-          'staff.1.name': ['The name field is required.'],
-        },
-      },
-    },
+    'staff.0.id': ['The id field is required.'],
+    'staff.0.name': ['The name field is required.'],
+    'staff.1.id': ['The id field is required.'],
+    'staff.1.name': ['The name field is required.'],
   }
   const defaultErrMsg = {
     staff: {
@@ -117,19 +89,12 @@ test('convert nested iterative errors with default errMsg', t => {
 
 test('convert various errors', t => {
   const err = {
-    response: {
-      status: 422,
-      data: {
-        errors: {
-          'address.prefecture': ['The prefecture field is required.'],
-          'address.zipcode': ['The zipcode field is required.'],
-          'staff.0.id': ['The id field is required.'],
-          'staff.0.name': ['The name field is required.'],
-          'staff.1.id': ['The id field is required.'],
-          'staff.1.name': ['The name field is required.'],
-        },
-      },
-    },
+    'address.prefecture': ['The prefecture field is required.'],
+    'address.zipcode': ['The zipcode field is required.'],
+    'staff.0.id': ['The id field is required.'],
+    'staff.0.name': ['The name field is required.'],
+    'staff.1.id': ['The id field is required.'],
+    'staff.1.name': ['The name field is required.'],
   }
   t.deepEqual(convertValidationError(err), {
     address: {
